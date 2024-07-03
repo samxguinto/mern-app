bdfe2ca5d3775994c81a688bb5f71e76bd66eb0c
spammyg413 <spamguinto@gmail.com>
2024-07-03 16:36:05 -0700

    working on front end finished most of backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
 const express = require ('express')
+const workoutRoutes = require('./routes/workouts');
+
 
 //express app
 const app = express()
 
+// middleware
+app.use(express.json())
+
+app.use((req, res, next ) => {
+    console.log(req.path, req.method )
+    next()
+})
+
 // routes
-app.get('/', (req, res) =>{
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
-//listen for requests
-app.listen(4000, ()  => {
-    console.log('listening on port 4000')
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+app.listen(process.env.PORT, ()  => {
+    console.log('connected to db and listening on port', process.env.PORT)
 })
-
+    })
+    .catch((error) => {
+        console.log(error);
+    })
