bf85f1465bf61024a564f265caf4fb6734c89fd7
spammyg413 <spamguinto@gmail.com>
2024-07-16 15:15:40 -0700

    just finished context updater

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,36 @@
+import { useEffect, } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+//components
+
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm";
+
+
 const Home = () => {
+    const {workouts, dispatch} = useWorkoutsContext();
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if(response.ok){
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
     return(
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) =>(
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
+            <WorkoutForm />
         </div>
     )
 }
